09bba498952261fd7f3f795861a089fbe10d5ae7
Hubert Ramsauer <hubi@Huberts-MacBook-Pro.local>
2023-02-12 13:24:22 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='curious_optimgan',
+    version='0.0.1',
+    description='OptimGAN with curiosity',
+    py_modules=['optimgan'],
+    package_dir={'': 'src'},
+)
