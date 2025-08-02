9ea8caa1c447c333fe0d692e0bf09ca1b27b2ff2
Hubert Ramsauer <hubert.ramsauer@gmail.com>
2025-08-02 20:19:18 +0200

    add pix2pix to init

--- src/gfog/models/__init__.py
+++ src/gfog/models/__init__.py
@@ -1,5 +1,8 @@
 from .mlp import MLP
 from .dcgan import DCDiscriminator
 from .dcgan import DCGenerator
+from .pix2pix_models import define_D
+from .pix2pix_models import define_G
 
-__all__ = ["MLP",  "DCDiscriminator", "DCGenerator"]
+__all__ = ["MLP",  "DCDiscriminator", "DCGenerator", "define_D", "define_G"]
+
